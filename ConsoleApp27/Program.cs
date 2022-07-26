42ca8daef2760f47ac92b478b0f82b73b069d460
admin <admin@CHETUIWK1860>
2022-07-26 12:03:23 +0530

    Add project files.

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        int x, y;
+
+        public Program()
+        {
+            x = 100;
+            y = 175;
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.WriteLine(p.x);
+            Console.WriteLine(p.y);
+            Console.ReadLine();
+        }
+    }
+}
